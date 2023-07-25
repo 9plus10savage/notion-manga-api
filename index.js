bc84e844e18c1f3a60e49547426729e7a9394ae2
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2023-07-25 02:10:56 +0200

    first draft
    updated read functionality for json + added write functionality.
    created function for getting manga stats
    will refactor

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
-require('dotenv').config();
 const axios = require('axios')
-const fs = require('fs')
 const Manga = require('./class.js')
-const mangaList = require('./reading-list.json')
+const { writeFile, readFileSync } = require("fs");
 
 const title = 'Blue Lock';
 const baseUrl = 'https://api.mangadex.org';
+const jsonPath = './reading_list.json'
 
 const order = {
     relevance: 'desc',
     followedCount: 'desc'
 }
 
-// define how query results are ordered
 const finalOrderQuery = {};
 for (const [key, value] of Object.entries(order)) {
     finalOrderQuery[`order[${key}]`] = value;
 };
+
+async function getMangaStats(resp)  {
+    mangaTitle = resp.data.data[0].attributes.title.en
+    mangaID = resp.data.data[0].id
+    latestChapter = await chapterQuery(resp.data.data[0].attributes.latestUploadedChapter)
+    return new Manga(mangaTitle, mangaID, latestChapter)
+}
+
+ const updateList = (manga) => {
+    const reading_list = JSON.parse(readFileSync(jsonPath))
+    reading_list['mangas'].push(manga)
+    writeFile(jsonPath, JSON.stringify(reading_list, null, 2), (err) => {
+        if (err) {
+            console.log("Failed to write updated data to file");
+            return;
+        }
+        console.log("Updated file successfully");
+    });
+ };
 
 const chapterQuery = async (chapterID) => {
     const resp = await axios({
@@ -35,13 +52,10 @@ const mangaQuery = async () => {
             ...finalOrderQuery
         }  
     });
-    mangaTitle = resp.data.data[0].attributes.title.en
-    mangaID = resp.data.data[0].id
-    latestChapter = await chapterQuery(resp.data.data[0].attributes.latestUploadedChapter)
-    const manga = new Manga(mangaTitle, mangaID, latestChapter)
-    console.log(JSON.stringify(manga))
+ 
+    const manga = await getMangaStats(resp)
+    updateList(manga)
 }
-
 
 if (typeof require !== 'undefined' && require.main === module) {
     mangaQuery();
