83b54e2952d5d82984e98a97945776520631f2e7
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2023-07-25 19:38:07 +0200

    refactor for async + prep for validation

--- index.js
+++ index.js
@@ -1,44 +1,62 @@
 const axios = require('axios')
 const Manga = require('./class.js')
-const { writeFile, readFileSync } = require("fs");
+const { writeFile, readFile } = require("fs");
 
-const title = 'Blue Lock';
+const title = 'Berserk';
 const baseUrl = 'https://api.mangadex.org';
 const jsonPath = './reading_list.json'
 
-const order = {
-    relevance: 'desc',
-    followedCount: 'desc'
-}
 
-const finalOrderQuery = {};
-for (const [key, value] of Object.entries(order)) {
-    finalOrderQuery[`order[${key}]`] = value;
-};
+const getOrder = () => {
+    const order = {
+        relevance: 'desc',
+        followedCount: 'desc'
+    }
+    
+    const finalOrderQuery = {};
+    for (const [key, value] of Object.entries(order)) {
+        finalOrderQuery[`order[${key}]`] = value;
+    };
+    return finalOrderQuery
+}
 
 async function getMangaStats(resp)  {
-    mangaTitle = resp.data.data[0].attributes.title.en
-    mangaID = resp.data.data[0].id
-    latestChapter = await chapterQuery(resp.data.data[0].attributes.latestUploadedChapter)
+    [mangaTitle, mangaID, chapterID] = [resp.data.data[0].attributes.title.en, resp.data.data[0].id, resp.data.data[0].attributes.latestUploadedChapter]
+    latestChapter = await chapterQuery(chapterID)
+    console.log(resp.data.data[0].attributes)
     return new Manga(mangaTitle, mangaID, latestChapter)
+    
 }
 
- const updateList = (manga) => {
-    const reading_list = JSON.parse(readFileSync(jsonPath))
-    reading_list['mangas'].push(manga)
+ const updateList = (path, manga) => {
+    let reading_list;
+    readFile(path, (error, jsonString) => {
+        if (error) {
+            console.log(error);
+            return;
+        }
+        try {
+            reading_list = JSON.parse(jsonString)
+            console.log(reading_list['mangas'].map(a => a.title))
+            reading_list['mangas'].push(manga)
+        } catch (err) {
+            console.log("Error parsing JSON string:", err);
+        }
+
     writeFile(jsonPath, JSON.stringify(reading_list, null, 2), (err) => {
         if (err) {
             console.log("Failed to write updated data to file");
             return;
         }
         console.log("Updated file successfully");
+        });
     });
  };
 
-const chapterQuery = async (chapterID) => {
+const chapterQuery = async (chapterID_to_query) => {
     const resp = await axios({
         method: 'GET',
-        url: `${baseUrl}/chapter/${chapterID}`
+        url: `${baseUrl}/chapter/${chapterID_to_query}`
     });
     return resp.data.data.attributes.chapter
 } 
@@ -49,12 +67,11 @@ const mangaQuery = async () => {
         url: `${baseUrl}/manga`,
         params: {
             title: title,
-            ...finalOrderQuery
+            ...getOrder()
         }  
     });
- 
     const manga = await getMangaStats(resp)
-    updateList(manga)
+    updateList(jsonPath, manga)
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
