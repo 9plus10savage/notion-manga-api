488a739a30e9486a8e783c7e68a5a0cfab53af2d
Gabriel Høst Andersen <gabrielandersen1@gmail.com>
2023-11-13 16:59:44 +0100

    create syncing with notion functionality

--- mangadex_api/manga-overview.js
+++ mangadex_api/manga-overview.js
@@ -3,6 +3,7 @@
 const { addManga } = require('./add-manga');
 const { deleteManga } = require('./delete-manga');
 const { updateManga, loadJSONFile } = require('./update-manga');
+const {updateMangaChapterNotion} = require('../notion_api/notion-update')
 const inquirer = require('inquirer')
 const path = require('path')
 const jsonPath = path.join(path.normalize(path.join(path.dirname(require.main.filename), '..')), 'reading_list.json')
@@ -14,6 +15,7 @@ const mangaCli = async () => {
         deleteManga: 'Delete manga',
         updateManga: 'Check for release updates',
         showMangaList: 'Display reading list',
+        postToNotion: 'Sync reading list to Notion',
         exit: 'Exit'
     }
 
@@ -22,7 +24,7 @@ const mangaCli = async () => {
             name: 'choice',
             message: 'Welcome to the manga reading list database, sourced from mangadex.org! Please select which functionality you wish to access:',
             type: 'list',
-            choices: [choices.addManga, choices.deleteManga, choices.updateManga, choices.showMangaList, choices.exit]
+            choices: [choices.addManga, choices.deleteManga, choices.updateManga, choices.showMangaList, choices.postToNotion, choices.exit]
         }
     ]);
 
@@ -34,16 +36,40 @@ const mangaCli = async () => {
             await deleteManga();
             break;
         case choices.updateManga:
-            await updateManga();
-            break;
+            case choices.updateManga:
+                updateManga().then(() => {
+                    return new Promise(resolve => {
+                        setTimeout(() => {
+                            resolve(
+                                inquirer.prompt([
+                                    {
+                                        name: 'postToNotion',
+                                        message: 'Would you like to sync the reading list to Notion?',
+                                        type: 'confirm',
+                                        default: false
+                                    }
+                                ])
+                            );
+                        }, 300);
+                    });
+                }).then(async (answers) => {
+                    if (answers.postToNotion) {
+                        const data = await loadJSONFile(jsonPath);
+                        await updateMangaChapterNotion(data.mangas);
+                    }
+                });
+                break;
         case choices.showMangaList:
             try {
                 const { mangas } = await loadJSONFile(jsonPath);
-                console.log('last fire mbyy')
                 console.log(mangas);
             } catch (error) {
                 console.error('Error loading the reading list:', error.message);
             }
+            break;
+        case choices.postToNotion:
+            const data = await loadJSONFile(jsonPath);
+            await updateMangaChapterNotion(data.mangas);
             break;
         case choices.exit:
             process.exitcode = 0
--- notion_api/notion-update.js
+++ notion_api/notion-update.js
@@ -1,24 +1,61 @@
-require('dotenv').config({path: 'notion.env'})
-const baseURL = "https://api.notion.com"
-
+const fs = require('fs')
 const { Client } = require("@notionhq/client")
+require('dotenv').config({path: 'notion.env'})
+
+const readingList = JSON.parse(fs.readFileSync('../reading_list.json', 'utf-8'))
 
 const notion = new Client({
   auth: process.env.NOTION_TOKEN,
-})
+});
 
-const getUsers = async () => {
-	const listUsersResponse = await notion.users.list({})
-    console.log(listUsersResponse)
+
+const getPageIDbyTitle = async (title) => {
+    const response = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+      filter: {
+        "property": "Title",
+        "rich_text": {
+          "equals": title
+        }
+      }
+    });
+    return response.results.map(manga => manga.id)
+  };
+
+
+  const updateSingleManga = async (manga) => {
+    try {
+      const [pageID] = await getPageIDbyTitle(manga.title);
+      const response = await notion.pages.update({
+        page_id: pageID,
+        properties: {
+          "Chapters (T)": {
+            "number": Number(manga.latestChapter)
+          }
+        }
+      });
+
+      if (require.main === module) {      
+          console.log('[Operation successful] Updated ' + manga.title + ' to chapter ' + manga.latestChapter);
+}     } catch (error) {
+          console.error(`Failed to update ${manga.title}: ${error}`);
+    }
+  }
+  
+  const delay = ms => new Promise(res => setTimeout(res, ms));
+
+  const updateMangaChapterNotion = async (mangaListToUpdate) => {
+    console.log("Now updating reading list to Notion...")
+    for (const manga of mangaListToUpdate) {
+      await updateSingleManga(manga);
+      await delay(1000 / 3); // Delay to respect the rate limit of 3 requests per second
+    }
+    console.log("Reading list has been synced to Notion!")
+  }
+  
+  
+if (require.main === module) {
+    updateMangaChapterNotion(readingList.mangas)
 }
 
-(async () => {
-    const response = await notion.databases.query({ database_id: process.env.NOTION_DB_ID, filter: { 
-        "property": "Favorites",
-        "checkbox": {
-            "equals": true
-       } } }
-       );
-    console.log(response.results.map((manga => manga.properties.Title.title[0].plain_text)))
-    console.log(response.results[0].properties.Title.title[0].plain_text);
-  })();
+module.exports = { updateMangaChapterNotion }
